23404c07c38df0bb2702621dc5370190ac8a82ef
Maurice Achtenhagen <maurice@blockchain.com>
2018-07-30 15:32:45 -0400

    feat(KYC): improve personal details screen

--- Blockchain/KYC/KYCPersonalDetailsController.swift
+++ Blockchain/KYC/KYCPersonalDetailsController.swift
@@ -13,21 +13,43 @@ final class KYCPersonalDetailsController: UIViewController, KYCOnboardingNavigat
 
     // MARK: - Properties
 
-    var segueIdentifier: String?
+    private let birthdayFieldGesture: UIGestureRecognizer!
+    var segueIdentifier: String? = "verifyMobileNumber"
 
     // MARK: - IBOutlets
 
+    @IBOutlet var firstNameField: UITextField!
+    @IBOutlet var lastNameField: UITextField!
+    @IBOutlet var birthdayField: UIView!
     @IBOutlet var primaryButton: PrimaryButton!
+
+    // MARK: - Initialization
+
+    required init?(coder aDecoder: NSCoder) {
+        birthdayFieldGesture = UIGestureRecognizer(target: birthdayField, action: #selector(showBirthdayPicker))
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: - Private Methods
+
+    @objc private func showBirthdayPicker() {
+        print("showing birthday picker")
+    }
 
     // MARK: - Actions
 
     @IBAction func primaryButtonTapped(_ sender: Any) {
-        let verifyAccountController = UIStoryboard.instantiate(
-            child: KYCVerifyAccountController.self,
-            from: KYCOnboardingController.self,
-            in: UIStoryboard(name: "KYCOnboardingScreen", bundle: nil),
-            identifier: "OnboardingScreen"
-        )
-        self.navigationController?.pushViewController(verifyAccountController, animated: true)
+        performSegue(withIdentifier: segueIdentifier!, sender: self)
+//        let verifyAccountController = UIStoryboard.instantiate(
+//            child: KYCVerifyAccountController.self,
+//            from: KYCOnboardingController.self,
+//            in: UIStoryboard(name: "KYCOnboardingScreen", bundle: nil),
+//            identifier: "OnboardingScreen"
+//        )
+//        self.navigationController?.pushViewController(verifyAccountController, animated: true)
     }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
 }
