ae9180994fac270c3accc622038d5baad3766d6e
Maurice Achtenhagen <maurice@blockchain.com>
2018-07-31 15:06:14 -0400

    feat(KYC): add birthdate picker

--- Blockchain/KYC/KYCPersonalDetailsController.swift
+++ Blockchain/KYC/KYCPersonalDetailsController.swift
@@ -13,40 +13,77 @@ final class KYCPersonalDetailsController: UIViewController, KYCOnboardingNavigat
 
     // MARK: - Properties
 
-    private let birthdayFieldGesture: UIGestureRecognizer!
+    private let birthdatePicker: UIDatePicker!
     var segueIdentifier: String? = "verifyMobileNumber"
 
     // MARK: - IBOutlets
 
     @IBOutlet var firstNameField: UITextField!
     @IBOutlet var lastNameField: UITextField!
-    @IBOutlet var birthdayField: UIView!
+    @IBOutlet var birthdateField: UITextField!
     @IBOutlet var primaryButton: PrimaryButton!
 
-    // MARK: - Initialization
+    override func viewDidLoad() {
+        setUpBirthdatePicker()
+        birthdateField.inputView = birthdatePicker
+    }
 
     required init?(coder aDecoder: NSCoder) {
-        birthdayFieldGesture = UIGestureRecognizer(target: birthdayField, action: #selector(showBirthdayPicker))
+        birthdatePicker = UIDatePicker()
+        birthdatePicker.datePickerMode = .date
+        birthdatePicker.maximumDate = Date()
         super.init(coder: aDecoder)
     }
 
     // MARK: - Private Methods
 
-    @objc private func showBirthdayPicker() {
-        print("showing birthday picker")
+    private func setUpBirthdatePicker() {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(submitBirthdate))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(hideBirthdatePicker))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+
+        birthdateField.inputAccessoryView = toolBar
+    }
+
+    @objc private func submitBirthdate() {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        birthdateField.text = dateFormatter1.string(from: birthdatePicker.date)
+        birthdateField.resignFirstResponder()
+    }
+
+    @objc private func hideBirthdatePicker() {
+        birthdateField.resignFirstResponder()
     }
 
     // MARK: - Actions
 
     @IBAction func primaryButtonTapped(_ sender: Any) {
+        let dateOfBirth = birthdatePicker.date
+        let calendar = Calendar(identifier: .gregorian)
+        let age = calendar.dateComponents([.year], from: dateOfBirth, to: Date())
+        guard let year = age.year, year >= 18 else {
+            AlertViewPresenter.shared.standardNotify(
+                message: "You must be at least 18 years old to have your identity verified",
+                title: "A bit too young",
+                actions: [
+                    UIAlertAction(title: "OK, I understand", style: .default, handler: { _ in
+                        // TODO: exit KYC flow and send user back to dashboard
+                    })
+                ],
+                in: self
+            )
+            return
+        }
         performSegue(withIdentifier: segueIdentifier!, sender: self)
-//        let verifyAccountController = UIStoryboard.instantiate(
-//            child: KYCVerifyAccountController.self,
-//            from: KYCOnboardingController.self,
-//            in: UIStoryboard(name: "KYCOnboardingScreen", bundle: nil),
-//            identifier: "OnboardingScreen"
-//        )
-//        self.navigationController?.pushViewController(verifyAccountController, animated: true)
     }
 
     // MARK: - Navigation
